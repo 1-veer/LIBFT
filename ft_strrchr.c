0b879dd589e40c58035cad8e160307a7f3696ce6
Ayoub Bougatia <abougati@e1r9p10.1337.ma>
2024-11-05 20:09:44 +0100

    edits

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,30 +6,49 @@
 /*   By: abougati <abougati@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 23:09:08 by abougati          #+#    #+#             */
-/*   Updated: 2024/11/05 12:58:33 by abougati         ###   ########.fr       */
+/*   Updated: 2024/11/05 19:37:21 by abougati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// #include "libft.h"
+
+// char	*ft_strrchr(const char *s, int c)
+// {
+// 	int	i;
+
+// 	i=0;
+// 	while (s[i])
+// 		i++;
+// 	s += i;
+// 	if ((char)c == '\0')
+// 		return ((char *)s);  // return s pointing on null , cuz s is on the null terminator atm
+// 	while (i-- >= 0)
+// 	{
+// 		s--;
+// 		if (*s == (char)c)      //decrement then compare
+// 			return ((char *)s);
+// 	}
+// 	return (NULL);
+// }
 
 #include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int	i;
+	const char *last_occurrence = NULL;
 
-	i=0;
-	while (s[i])
-		i++;
-	s += i;
-	if (c == '\0')
-		return ((char *)s);  // return s pointing on null , cuz s is on the null terminator atm
-	while (i-- >= 0)
+	// Traverse the string
+	while (*s)
 	{
-		if (*--s == (char)c)      //decrement then compare
-			return ((char *)s);
+		if (*s == (char)c)
+			last_occurrence = s; // Update last occurrence whenever c is found
+		s++;
 	}
-	if ((char)c == '\0')
-		return ((char *)s);	
-	
-	return (NULL);
-}
 
+	// If c is '\0', return a pointer to the null terminator
+	if ((char)c == '\0')
+		return ((char *)s);
+
+	// Return last occurrence of c, or NULL if not found
+	return ((char *)last_occurrence);
+}
